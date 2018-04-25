71f5f2176f6259a5d85022959dd7fe52edd3e866
Haifeng Su <spy.haifeng@gmail.com>
2018-04-25 17:39:12 -0400

    Final Project Submission (Completed)
    -Functional Menu added

--- GameBase-Space-X-Defense-Program/Space-X Defense Program/Space-X Defense Program/GameViewController.swift
+++ GameBase-Space-X-Defense-Program/Space-X Defense Program/Space-X Defense Program/GameViewController.swift
@@ -53,11 +53,12 @@ class GameViewController: UIViewController {
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        //let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        let scene = SKScene(fileNamed: "MainMenuScene")
         let skView = self.view as! SKView
         skView.showsFPS = true
         skView.showsNodeCount = true
-        scene.scaleMode = .aspectFill
+        scene?.scaleMode = .aspectFill
         skView.presentScene(scene)
     }
     
--- GameBase-Space-X-Defense-Program/Space-X Defense Program/Space-X Defense Program/MainMenuScene.swift
+++ GameBase-Space-X-Defense-Program/Space-X Defense Program/Space-X Defense Program/MainMenuScene.swift
@@ -43,5 +43,19 @@ class MainMenuScene: SKScene {
             }
         }
         
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 3.0),
+            SKAction.run() {
+                // 5
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+                self.view?.presentScene(scene, transition:reveal)
+            }
+            ]))
+        
     }
+    
+   
+    
+    
 }
